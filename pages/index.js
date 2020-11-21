7caf849161e737ec57b4c5f8d3eb837ff822ac23
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2020-11-20 23:03:57 -0400

    fix name

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to EPAYCO
+          Welcome to ePAYco
         </h1>
 
         <div className="grid">
